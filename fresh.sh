2cfdcbf8808dee2a9382da6d7f9252a7099074dd
TJ Moyer <tj@mtvernonmedia.com>
2023-03-08 11:06:16 -0500

    Update fresh.sh

--- fresh.sh
+++ fresh.sh
@@ -35,7 +35,7 @@ brew tap homebrew/bundle
 brew bundle --file ./Brewfile
 
 # Set default MySQL root password and auth type
-mysql -u root -e "ALTER USER root@localhost IDENTIFIED WITH mysql_native_password BY 'password'; FLUSH PRIVILEGES;"
+#mysql -u root -e "ALTER USER root@localhost IDENTIFIED WITH mysql_native_password BY 'password'; FLUSH PRIVILEGES;"
 
 # Install PHP extensions with PECL
 pecl install imagick redis swoole
